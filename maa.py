bbe5759d36b3a8b0363d539e435bf87a8621aad6
princekumargiri <162794324+princekumargiri@users.noreply.github.com>
2024-03-10 18:51:39 +0530

    print number
    list coding

---
+++ maa.py
@@ -0,0 +1,6 @@
+list1= [12,-7,5,64,-14]
+positive_number=[ num for num in list1 if num>0]
+print(positive_number)
+list2=[12,14,-95,3]
+positive_number=[ num for num in list2 if num>0]
+print(positive_number)                 
